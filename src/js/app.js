c0be51d618651cb3265ca3b4d5f1b24e8726a2da
vertalet76crew <vertalet76crew@gmail.com>
2020-09-07 14:35:05 +0400

    finish

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 import $ from "jquery";
 import menu from "./blocks/menu";
-import formSubscription from "./blocks/form-subscription";
+
 import formRequest from "./blocks/form-request";
 import newsCarousel from "./blocks/news";
 import faq from "./blocks/faq";
@@ -10,7 +10,6 @@ $(function () {
   const patternUserAgent = /iPhone|iPod|iPad|iOS|android/i;
   const isMobile = !!navigator.userAgent.match(patternUserAgent);
   menu(isMobile);
-  formSubscription();
   formRequest(isMobile);
   newsCarousel();
   faq();
