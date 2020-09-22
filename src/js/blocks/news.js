5b616351d647a8c8c30e86fd4db94d9fe5edd7ce
vertalet76crew <vertalet76crew@gmail.com>
2020-09-22 13:47:09 +0400

    WIP

--- src/js/blocks/news.js
+++ src/js/blocks/news.js
@@ -2,27 +2,7 @@ import $ from 'jquery';
 import 'owl.carousel';
 
 function newsCarousel() {
-    let offsetLeft;
-    function computedPosition() {
-        const $faqWrap = $(".faq_wrap")
-        const positionStyle = (style) => parseInt($faqWrap.css(style))
-        const rect = {
-            width: $faqWrap.width(),
-            height: $faqWrap.height(),
-            paddingLeft: positionStyle("padding-left"),
-            paddingRight: positionStyle("padding-right"),
-            marginRight: positionStyle("margin-right"),
-            marginLeft: positionStyle("margin-left"),
-            // resulPosition: rect.width + rect.marginRight + rect.marginLeft
-        }
-        const resulPosition = rect.width + rect.marginLeft - rect.paddingRight - rect.marginRight
-
-        offsetLeft = $faqWrap.offset().left + rect.paddingRight - 20
-
-    }
     const $slider = $('[data-carousel="news"]');
-    $(window).on("resize", computedPosition)
-    $(window).on("load", computedPosition)
 
     if ($slider.length) {
         $slider.owlCarousel({
@@ -34,7 +14,7 @@ function newsCarousel() {
             margin: 0,
             items: 1,
             smartSpeed: 700,
-            stagePadding: 20,
+            stagePadding: 20
         });
     }
 }
